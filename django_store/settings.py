6add6f2831c5e329d655d9a112d2370b7055edd0
khaled21developer <kh.hammad2022@gmail.com>
2024-11-12 15:07:44 +0200

    تحميل محتوى الصفحات من قاعدة البيانات - الجزء الأول

--- django_store/settings.py
+++ django_store/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
+    'checkout',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,9 @@ ROOT_URLCONF = 'django_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # تصنيفات بشكل ديناميكي في كل الصفحات
+                'store.custom_context_processor.store_website',
             ],
         },
     },
@@ -111,11 +118,19 @@ USE_I18N = True
 
 USE_TZ = True
 
+# لاظهار وحفظ الصور في المجلد
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# اعدادات المشروع لاضافة القالب وتفعيله على المتصفح بمظهر جيد
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_per_page = 20
+
+@admin.register(models.Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_per_page = 20
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_per_page = 20
+
+
+@admin.register(models.Slider)
+class SliderAdmin(admin.ModelAdmin):
+    list_per_page = 20
