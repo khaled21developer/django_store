f46fca537dbec8ff00f024ef71fb6aa181d3e548
khaled21developer <kh.hammad2022@gmail.com>
2024-12-09 09:59:33 +0200

    {بوابات الدفع والخطافات وادارة الطلبات}

--- store/admin.py
+++ store/admin.py
@@ -23,4 +23,28 @@ class SliderAdmin(admin.ModelAdmin):
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'amount', 'payment_method', 'items', 'created_at']
     list_per_page = 20
+    list_select_related = ['transaction']
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def amount(self, obj):
+        return obj.transaction.amount if obj.transaction else None
+
+    def items(self, obj):
+        return len(obj.transaction.items) if obj.transaction and obj.transaction.items else None
+
+    def email(self, obj):
+        return obj.transaction.customer_email if obj.transaction else None
+
+    def payment_method(self, obj):
+        return obj.transaction.get_payment_method_display() if obj.transaction else None
+
+
+
+
